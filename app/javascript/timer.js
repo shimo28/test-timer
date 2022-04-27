0d6dccfe1dd2a8afd496518e66c95664d1c4a363
shimo28 <shun20000816@gmail.com>
2022-04-28 08:08:00 +0900

    １分timer

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -2,16 +2,14 @@ window.addEventListener('load', function(){
   const start = document.querySelector("#start");
   const time = document.querySelector("#time");
   const count = 1000;
-  let timer = 1500;
+  let timer = 60;
   let min;
   let sec;
-  let start_f = false;
 
   start.addEventListener('click',count_start, false);
   function count_start(){
-    if (start_f === false) {
+    start.disabled = true;
       const set_id =setInterval(function(){
-        start_f = true;
         if(timer <= 0 ){
           clearInterval(set_id);
         }else{
@@ -21,6 +19,6 @@ window.addEventListener('load', function(){
           time.innerHTML = min +":" + ("0"+sec).slice(-2);
         }
       },count);
-    }
+    
   }
 });
