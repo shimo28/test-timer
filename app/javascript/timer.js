fd81e475f5efb64c1edd22a09fa9339059450c92
shimo28 <shun20000816@gmail.com>
2022-04-28 11:29:45 +0900

    6秒timer

--- app/javascript/timer.js
+++ app/javascript/timer.js
@@ -2,9 +2,13 @@ window.addEventListener('load', function(){
   const start = document.querySelector("#start");
   const time = document.querySelector("#time");
   const count = 1000;
-  let timer = 60;
+  let timer = 6;
   let min;
   let sec;
+
+  min = Math.floor(timer / 60);
+  sec = timer % 60;
+  time.innerHTML = ("0"+min) +":" + ("0"+sec).slice(-2);
 
   start.addEventListener('click',count_start, false);
   function count_start(){
@@ -12,11 +16,16 @@ window.addEventListener('load', function(){
       const set_id =setInterval(function(){
         if(timer <= 0 ){
           clearInterval(set_id);
+          timer = 6;
+          start.disabled = false;
+          min = Math.floor(timer / 60);
+          sec = timer % 60;
+          time.innerHTML = ("0"+min) +":" + ("0"+sec).slice(-2);
         }else{
           timer--;
           min = Math.floor(timer / 60);
           sec = timer % 60;
-          time.innerHTML = min +":" + ("0"+sec).slice(-2);
+          time.innerHTML = ("0"+min) +":" + ("0"+sec).slice(-2);
         }
       },count);
     
